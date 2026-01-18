00a82e6bf9cedb513d75f2d41cd231c39c9440a8
guyn123 <quanhhth492005@gmail.com>
2026-01-18 22:09:10 +0700

    add v1

---
+++ Assets/Blinking.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Blinking : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+
+    // Start được gọi 1 lần khi bắt đầu game
+    void Start()
+    {
+        // Lấy thành phần quản lý hình ảnh của ngọn lửa
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update được gọi liên tục mỗi khung hình
+    void Update()
+    {
+        // Đảo ngược trạng thái hiển thị (Đang Bật -> Tắt, Đang Tắt -> Bật)
+        spriteRenderer.enabled = !spriteRenderer.enabled;
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Update được gọi liên tục mỗi khung hình
+    void Update()
+    {
+        // 1. Lấy vị trí chuột và đổi từ Tọa độ màn hình (Screen) sang Tọa độ game (World)
+        var worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // 2. Đặt lại Z = 0 (Bắt buộc vì đây là game 2D phẳng)
+        worldPoint.z = 0;
+
+        // 3. Gán vị trí của tàu vào vị trí mới vừa tính toán
+        transform.position = worldPoint;
+    }
+}
