3206f81b88ae60466bfb1cbbcb93edcc9ba7ec2e
guyn123 <quanhhth492005@gmail.com>
2026-01-23 14:41:52 +0700

    Demo_part2

---
+++ Assets/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+    public float flySpeed = 10; // Tốc độ bay của đạn
+
+    void Update()
+    {
+        // Làm đạn bay lên trên theo trục Y
+        var newPosition = transform.position;
+        newPosition.y += Time.deltaTime * flySpeed;
+        transform.position = newPosition;
+    }
+}
---
+++ Assets/PlayerShootingg.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShootingg : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float shootingInterval = 0.2f; // Thời gian giãn cách giữa 2 viên đạn
+    private float lastBulletTime;
+
+    void Update()
+    {
+        // Dùng GetMouseButton (giữ chuột) thay vì GetMouseButtonDown (nhấn 1 cái)
+        if (Input.GetMouseButton(0))
+        {
+            // Kiểm tra xem đã đủ thời gian chờ chưa
+            if (Time.time - lastBulletTime > shootingInterval)
+            {
+                ShootBullet();
+                lastBulletTime = Time.time;
+            }
+        }
+    }
+
+    void ShootBullet()
+    {
+        Instantiate(bulletPrefab, transform.position, transform.rotation);
+    }
+}
